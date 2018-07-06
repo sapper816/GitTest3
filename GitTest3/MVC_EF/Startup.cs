5ee6f064d9ae6626935192f1833616450e9cb9bf
Preston Keough <sapper816@hotmail.com>
2018-07-06 11:05:01 -0500

    Created Project

---
+++ GitTest3/MVC_EF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVC_EF.Startup))]
+namespace MVC_EF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
